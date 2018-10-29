ad04e752931d6f57bfc66bcad953774317e971ab
robbie greg culkin <rculkin@linux60421.dc.engr.scu.edu>
2018-10-28 21:48:18 -0700

    filter ids in interface and data.sql

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO listings VALUES (001, 'Mikes Bikes', 'Buy your bikes here!', 'Retail', '500 El Camino Real', 'United States', 'CA', '95053', 1, 'Mike Bicycleman', '2001');
+INSERT INTO listings VALUES (002, 'Johns Bikes', 'Buy your bikes here!', 'Retail', '500 El Camino Real', 'United States', 'CA', '95053', 0, 'John Bicycleman', '2001');
+INSERT INTO listings VALUES (003, 'SpiderMan Tacos', 'Theyre off the wall!', 'Restaurant', '500 El Camino Real', 'United States', 'CA', '95053', 1, 'Peter Parker', '2019');
+INSERT INTO listings VALUES (004, 'Batman Tacos', 'IM BATMAN (RACHAEAEELELLLL)', 'Restaurant', '500 El Camino Real', 'United States', 'CA', '95053', 1, 'Bruce Wayne', '2019');
+INSERT INTO listings VALUES (005, 'Bane Tacos', 'I WAS BORN IN THE CARNE ASADA', 'Restaurant', '500 El Camino Real', 'United States', 'CA', '95053', 0, 'Banathy', '2019');
+INSERT INTO listings VALUES (006, 'Failing Startup', 'We have 30 monthly active users!', 'Technology', '500 El Camino Real', 'United States', 'CA', '95053', 1, 'Steve Jobs', '2019');
