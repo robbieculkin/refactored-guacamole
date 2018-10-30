a822c01eaefa874ee9ba7c599ed1e53b0fcfe9e4
robbie greg culkin <rculkin@linux60411.dc.engr.scu.edu>
2018-10-30 00:15:43 -0700

    Adding buttons to accept and deny listings

--- admin.php
+++ admin.php
@@ -1,49 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-		  <meta charset="utf-8"/>
+		  <meta charset="utf-8">
+		  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		  <title>Alumni Business Directory</title>
 		  <meta name="viewport" content="width=device-width, initial-scale=1">
 		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
 </head>
 <body>
+		  <!--Navigation-->
 		  <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
 					 <ul class="navbar-nav">
-								<li class="nav-item">
-										  <b><a class="nav-link" href="home.php">Alumni Business Directory</a></b>
+								<li class="nav-item active">
+										  <b><a class="nav-link disabled" href="home.php">Alumni Business Directory</a></b>
 								</li>
 								<li class="nav-item">
 										  <a class="nav-link" href="new.php">New Submission</a>
 								</li>
-								<li class="nav-item active">
-										  <a class="nav-link disabled" href="admin.php">Admin</a>
+								<li class="nav-item">
+										  <a class="nav-link" href="admin.php">Admin</a>
 								</li>
 					 </ul>
 		  </nav>
 
+		  <!--Content-->
 		  <div class="container">
-					 <div class="jumbotron text-center">
-								<h1>Admin Login</h1>
+					 <div class="jumbotron" style="text-align:center">
+								<h1>Admin Listing Verification</h1>
+							
 					 </div>
 		  </div>
 
-		  <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-					 <div class="form-group w-25 mx-auto">
-								<label for="username">Username</label>
-								<input name="user" type="text" class="form-control">
-					 </div>
-					 <div class="form-group w-25 mx-auto">
-								<label for="password">Password</label>
-								<input name="pass" type="password" class="form-control">
-					 </div>
-					 <div class="form-group text-center">
-								<button type="submit" class="btn btn-primary">Login</button>
-					 </div>
-		  </form>
 
+		  <?php
+					 include 'sql_interface.php';
+
+					 if ($_SERVER["REQUEST_METHOD"]=="POST"){
+					 			if($_POST["name"]=="approve")
+								{
+									approve_listing($_POST["value"]);
+								} else{
+									delete_listing($_POST["value"]);
+								}
+					 }
+
+					 $ids = get_ids(0, 'ANY', 'ANY');
+					 
+					 foreach($ids['ID'] as $id){
+								echo "<div id=\"busn\" class=\"card w-75 mx-auto\">";
+								$listing = get_listing($id);
+								echo "<div class=\"card-body\">";
+								echo "<h3 class=\"card-title\">".$listing['NAME'][0]."</h3>";
+								echo "<p class=\"card-text\">".$listing['DESCRIPTION'][0]."</p>";
+								echo "<p class=\"card-text\">".$listing['TYPE'][0]."</p>";
+								echo "<p class=\"card-text\">".$listing['ADDRESS'][0].", ";
+								echo $listing['COUNTRY'][0].", ";
+								echo $listing['STATE'][0].", ";
+								echo $listing['ZIP'][0]."</p>";
+								echo "<form action=".$_SERVER['PHP_SELF']." method=\"post\"";
+								echo '<div class="form-group text-center"><button name="approve" value='.$id.' type="submit" class="btn btn-primary">Approve</button></div>';
+								echo '<div class="form-group text-center"><button name="delete" value='.$id.'type="submit" class="btn btn-primary">Delete</button></div>';
+								echo "</div>";
+								echo "</div>";
+								echo "<br>";						
+					 }
+		  ?>
 
 </body>
 </html>
