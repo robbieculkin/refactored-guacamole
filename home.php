fbe4398851c51b613b7e9fe905b243cf15fcafde
sally park <spark1@linux60421.dc.engr.scu.edu>
2018-11-27 00:50:13 -0800

    Added modal for user metadata and completed filter form

--- home.php
+++ home.php
@@ -25,7 +25,48 @@
 										  <a class="nav-link" href="login.php">Admin</a>
 								</li>
 					 </ul>
+
+			  <!--User data button-->
+			  <div>
+						 <button name="userbtn" type="button" class="btn btn-primary" data-toggle="modal" data-target="#userData">Tell us about yourself!</button>
+			  </div>
 		  </nav>
+
+		  <!--Modal-->
+		  <div class="modal fade" id="userData" tabindex="-1" role="dialog" aria-labelledby="userDataLabel" aria-hidden="true">
+					 <div class="modal-dialog" role="document">
+								<div class="modal-content">
+										  <div class="modal-header">
+													 <h5 class="modal-title" id="exampleModalLabel">Tell us about yourself!</h5>
+										  </div>
+										  <div class="modal-body">
+													 <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+																<div class="form-group">
+																		  <label for="userName" class="col-form-label">Name (First Last):</label>
+																		  <input name="name" type="text" class="form-control" id="userName" value="none">
+																</div>
+																<div class="form-group">
+																		  <label for="gradYear" class="col-form-label">Graduation Year:</label>
+																		  <input name="year" type="text" class="form-control" id="gradYear" value="none">
+																</div>
+																<div class="form-group">
+																		  <label for="major" class="col-form-label">Major:</label>
+																		  <input name="major" type="text" class="form-control" id="major" value="none">
+																</div>
+																<div class="form-group">
+																		  <label for="reason" class="col-form-label">Why are you here?</label>
+																		  <input name="reason" type="text" class="form-control" id="reason" value="none">
+																</div>
+																<div class="form-group text-center">
+																		  <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+																		  <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+																</div>
+													 </form>
+										  </div>
+								</div>
+					 </div>
+		  </div>
+
 
 		  <!--Content-->
 		  <div class="container">
@@ -35,6 +76,10 @@
 								<input class="form-control" type="text" placeholder="Search" aria-label="Search">
 								-->
 								<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+										  <div class="form-group w-25 mx-auto">
+													 <label class="text-center" for="busType">Business Name</label>
+													 <input name="busname" type="text" class="form-control" id="busName" value="<?php if (isset($_POST['busname'])){echo $_POST['busname'];}else{echo "ANY";}?>">
+										  </div>
 										  <div class="form-group w-25 mx-auto">
 													 <label class="text-center" for="busType">Business Type</label>
 													 <select name="type" class="form-control" id="busType">
@@ -46,11 +91,37 @@
 													 </select>
 										  </div>
 										  <div class="form-group w-25 mx-auto">
+													 <label class="text-center" for="busType">Description (Full phrase)</label>
+													 <input name="desc" type="text" class="form-control" id="busDesc" value="<?php if (isset($_POST['desc'])){echo $_POST['desc'];}else{echo "ANY";}?>">
+										  </div>
+										  <div class="form-group w-25 mx-auto">
+													 <label class="text-center" for="busType">Address</label>
+													 <input name="addr" type="text" class="form-control" id="busAddr" value="<?php if (isset($_POST['addr'])){echo $_POST['addr'];}else{echo "ANY";}?>">
+										  </div>
+										  <div class="form-group w-25 mx-auto">
+													 <label class="text-center" for="busType">Country</label>
+													 <select name="country" class="form-control" id="busCountry">
+																<option <?php if (isset($_POST['country']) && $_POST['country']=="ANY"){echo "selected=true";}?>>ANY</option>
+																<option <?php if (isset($_POST['country']) && $_POST['country']=="USA"){echo "selected=true";}?>>USA</option>
+																<option <?php if (isset($_POST['country']) && $_POST['country']=="Canada"){echo "selected=true";}?>>Canada</option>
+																<option <?php if (isset($_POST['country']) && $_POST['country']=="Australia"){echo "selected=true";}?>>Australia</option>
+													 </select>
+										  </div>
+										  <div class="form-group w-25 mx-auto">
+													 <label class="text-center" for="busType">State</label>
+													 <select name="state" class="form-control" id="busState">
+																<option <?php if (isset($_POST['state']) && $_POST['state']=="ANY"){echo "selected=true";}?>>ANY</option>
+																<option <?php if (isset($_POST['state']) && $_POST['state']=="N/A"){echo "selected=true";}?>>N/A</option>
+																<option <?php if (isset($_POST['state']) && $_POST['state']=="California"){echo "selected=true";}?>>California</option>
+																<option <?php if (isset($_POST['state']) && $_POST['state']=="Washington"){echo "selected=true";}?>>Washington</option>
+													 </select>
+										  </div>
+										  <div class="form-group w-25 mx-auto">
 													 <label class="text-center" for="busType">ZIP Code</label>
 													 <input name="zip" type="text" class="form-control" id="busZip" value="<?php if (isset($_POST['zip'])){echo $_POST['zip'];}else{echo "ANY";}?>">
 										  </div>
 										  <div class="form-group text-center">
-													 <button name="submit" type="submit" class="btn btn-primary">Filter</button>
+													 <button name="searchsubmit" type="submit" class="btn btn-primary">Filter</button>
 										  </div>
 								</form>
 					 </div>
@@ -59,10 +130,13 @@
 					 include 'sql_interface.php';
 
 					 if ($_SERVER["REQUEST_METHOD"]=="POST"){
-								if (isset($_POST["submit"])){
-										  $ids = get_ids(1, 'ANY', 'ANY', $_POST['type'], 'ANY', 'ANY', 'ANY', $_POST['zip'], 'ANY', 'ANY', 'ANY');
-								}
-					 } else{
+								if (isset($_POST['searchsubmit'])){
+										  		$ids = get_ids(1, $_POST['busname'], $_POST['desc'], $_POST['type'], $_POST['addr'], $_POST['country'], $_POST['state'], $_POST['zip'], 'ANY', 'ANY', 'ANY');
+								} else{
+										  		$ids = get_ids(1);
+													insert_user(new_user_id(), $_POST['name'], $_POST['year'], $_POST['major'], $_POST['reason']);
+					 			}
+					 }else{
 								$ids = get_ids(1);
 					 }
 					 
